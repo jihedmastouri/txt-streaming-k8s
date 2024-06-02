4476c755e9f0353ae93373515c4a29cf7afe95eb
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2024-06-02 14:47:16 +0100

    cleaning the wip before opening

--- solution-2/server/app.py
+++ solution-2/server/app.py
@@ -1,1 +1,17 @@
-import bullmq
+from os import getenv
+
+redis_host = getenv("REDIS_HOST", "localhost")
+redis_port = int(getenv("REDIS_PORT", 6379))
+
+# maybe make async or use threads
+def main():
+    while True:
+        # maybe sleep
+        # redis lpop
+        # process request
+        # start publishing responses in redis
+        # maybe sleep again
+        pass
+
+if __name__ == "__main__":
+    main()

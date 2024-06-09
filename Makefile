ecd3ecdfc62ec150812fed100ca3b02f522d32ca
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2024-06-09 03:12:39 +0100

    working without k8s

--- Makefile
+++ Makefile
@@ -1,27 +1,37 @@
+.PHONY: redis
 redis:
-	docker run --name redis7 -d -p "6379:6379" redis:7.2
+	@docker start redis7 || docker run --name redis7 -d -p "6379:6379" redis:7.2
 
+.PHONY: docker-1
 docker-1:
-	docker build ./solution-1/client -t node-ms-sol1
-	docker build ./solution-1/server -t python-ms-sol1
+	@docker build ./solution-1/client -t node-ms-sol1
+	@docker build ./solution-1/server -t python-ms-sol1
 
+.PHONY: docker-2
 docker-2:
-	docker build ./solution-2/client -t node-ms-sol2
-	docker build ./solution-2/server -t python-ms-sol2
+	@docker build ./solution-2/client -t node-ms-sol2
+	@docker build ./solution-2/server -t python-ms-sol2
 
+.PHONY: apply-1
 apply-1:
 	kubectl apply -f ./solution-1/k8s
 
+.PHONY: apply-2
 apply-2:
+	kubectl apply -f ./solution-1/k8s
 
+.PHONY: alive-1
 alive-1:
 	curl 'http://localhost:3000/'
 
+.PHONY: alive-2
 alive-2:
 	curl 'http://localhost:3000/'
 
+.PHONY: req-1
 req-1:
 	curl -X POST -H "Content-Type: application/json" -d '{"data": "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."}' 'http://localhost:3000/'
 
-req-1:
+.PHONY: req-2
+req-2:
 	curl -X POST -H "Content-Type: application/json" -d '{"data": "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."}' 'http://localhost:3001/'
--- solution-1/client/index.ts
+++ solution-1/client/index.ts
@@ -2,11 +2,15 @@ import 'dotenv/config';
 import express from 'express';
 import { createClient } from 'redis';
 import axios from 'axios';
-import { makeid } from 'utils';
+import { makeid } from './utils';
 import { EventEmitter } from 'node:events';
 
-const client = createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+const clientRedisListen = createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
+
+const clientRedisPub = createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
 });
 
 const app = express();
@@ -15,57 +19,80 @@ app.use(express.json());
 const eventEmitter = new EventEmitter();
 
 app.post('/', async (req: any, res: any) => {
-  const b = await req.body;
+    const b = await req.body;
+    console.log(b);
 
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Connection', 'keep-alive');
-  res.flushHeaders();
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
 
-  const channelId = makeid();
+    const channelId = makeid();
 
-  client.LPUSH(`req-sol-1`, JSON.stringify({ id: channelId, data: b.data }));
+    clientRedisPub.LPUSH(
+        `req-sol-1`,
+        JSON.stringify({ id: channelId, data: b.data })
+    );
 
-  eventEmitter.once(channelId, (ip: string) => {
-    axios
-      .get(`${ip}/stream/${channelId}`, { responseType: 'stream' })
-      .then(({ data }: { data: string }) => {
-        if (data === '<end>') {
-          res.end();
-          return;
-        }
-        res.write(data);
-      })
-      .catch((err: any) => {
-        console.error(err);
-      });
-  });
+    res.write(`data: ${channelId}\n\n`);
+
+    eventEmitter.once(channelId, async (addr: string) => {
+        res.write(`data: ${addr}\n\n`);
+
+        const response = await axios.get(`${addr}/stream/${channelId}`, {
+            responseType: 'stream',
+        });
+
+        const stream = response.data;
+        stream.on('data', (data: any) => {
+            const text = new TextDecoder().decode(data);
+            console.log(text);
+            if (text === '<end>') {
+                res.end();
+                return;
+            }
+            res.write(text);
+        });
+
+        stream.on('end', () => {
+            console.log('stream done');
+        });
+
+        stream.on('error', (err: any) => {
+            console.error('stream error:', err);
+        });
+
+    });
 });
 
 async function main() {
-  await client.connect();
+    await clientRedisListen.connect();
+    await clientRedisPub.connect();
 
-  client.ping().then(() => {
-    console.log('Connected to Redis');
-  });
+    clientRedisListen.ping().then(() => {
+        console.log('Connected to Redis for listening');
+    });
+    clientRedisPub.ping().then(() => {
+        console.log('Connected to Redis for publishing');
+    });
 
-  client.subscribe(`res-sol-1`, (val) => {
-    console.log('Received message:', val);
-    let v;
-    try {
-      v = JSON.parse(val);
-    } catch (e) {
-      console.error('Error parsing message:', e);
-      return;
-    }
-    const { id, podIp } = v;
-    eventEmitter.emit(id, podIp);
-  });
+    clientRedisListen.subscribe(`res-sol-1`, (val) => {
+        console.log('Received message:', val);
+        let v;
+        try {
+            v = JSON.parse(val);
+        } catch (e) {
+            console.error('Error parsing message:', e);
+            return;
+        }
+        const { id, podAddr } = v;
+        eventEmitter.emit(id, podAddr);
+    });
 
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
 }
 
 main();
